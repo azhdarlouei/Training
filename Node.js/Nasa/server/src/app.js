861931e048e779c5700e3d7f0741320ae73c8805
Alireza <ar.azhdarloo@gmail.com>
2026-03-06 17:58:41 +0330

    Node.js - Nasa: make easy way to run

--- Node.js/Nasa/server/src/app.js
+++ Node.js/Nasa/server/src/app.js
@@ -1,4 +1,6 @@
 const express = require("express")
+const path = require("path")
+const morgan = require("morgan")
 const cors = require("cors")
 
 const planetsRouter = require("./routes/planets/planets.router")
@@ -8,7 +10,16 @@ const app = express()
 app.use(cors({
     origin: "http://localhost:3000"
 }))
+
+app.use(morgan('combined'))
+
 app.use(express.json())
+
+app.use(express.static(path.join(__dirname, "..", "public")))
+app.get("/", (req, res)=>{
+    res.sendFile(path.join(__dirname, "..", "public", "index.html"))
+})
+
 app.use(planetsRouter)
 
 module.exports = app

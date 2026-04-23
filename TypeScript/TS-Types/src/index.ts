3fe01dbaf9e7fb549d29d78ce5ce8ced97a6f20a
Alireza <ar.azhdarloo@gmail.com>
2026-04-23 19:09:52 +0330

    TypeScript - Ts-Types: arrays type

--- TypeScript/TS-Types/src/index.ts
+++ TypeScript/TS-Types/src/index.ts
@@ -3,3 +3,6 @@ let sales: number = 123_456_789;   // let sales = 123_456_789   type is Number
 let name: string = "Alireza";      // let name = "Alireza"      type is String
 let isPublished: boolean = true;   // let isPublished = true    type is Boolean
 let level;                         //                           type is 'any'
+
+// arrays
+let numbers: number[] = [1, 2, 3]  // let numbers = [1, 2, 3]   type is Number

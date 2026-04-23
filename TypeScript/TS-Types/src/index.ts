694764e8bf6a36ef07e9f17874c19336de909a23
Alireza <ar.azhdarloo@gmail.com>
2026-04-23 19:08:37 +0330

    TypeScript - Ts-Types: initial project and write about variables types

---
+++ TypeScript/TS-Types/src/index.ts
@@ -0,0 +1,5 @@
+// variables
+let sales: number = 123_456_789;   // let sales = 123_456_789   type is Number
+let name: string = "Alireza";      // let name = "Alireza"      type is String
+let isPublished: boolean = true;   // let isPublished = true    type is Boolean
+let level;                         //                           type is 'any'

d07eec2636e4e445bba2c68041f64d4ebe1ebeb9
Alireza <ar.azhdarloo@gmail.com>
2026-04-23 19:11:33 +0330

    TypeScript - Ts-Types: functions type

--- TypeScript/TS-Types/src/index.ts
+++ TypeScript/TS-Types/src/index.ts
@@ -13,3 +13,11 @@ let user: [number, string, boolean] = [1, "ALireza", true]
 // enums
 enum Size { Small = 1, Medium, Large }  // const Size { Small = 1, Medium, Large }  'is better to use'
 let mySize: Size = Size.Medium          // console.log(mySize) => 2
+
+// functions
+function calculateTax(income: number, taxYear = 2022): number {   // function calculateTax(income: number, taxYear?: number) {
+    if (taxYear < 2022)                                           //     if ((taxYear || 2022) < 2022)
+        return income * 1.2
+    return income * 1.3
+}                                                               /* If we dont have any type we dont need to add return in our function
+...                                                                because our function type is Void                                  */

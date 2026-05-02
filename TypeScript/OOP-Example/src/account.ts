deb019caf87dc3c94359c61cbecc8e373443f1e7
Alireza <ar.azhdarloo@gmail.com>
2026-05-02 15:11:56 +0330

    TypeScript - OOP-Example: access control key words

--- TypeScript/OOP-Example/src/account.ts
+++ TypeScript/OOP-Example/src/account.ts
@@ -1,22 +1,26 @@
 class Account {
     readonly id: number // it's can't change out of class
     owner: string
-    balance: number
+    private _balance: number // you can't see or change that out of class
     nickName?: string   // it's optional
 
-    constructor(id: number, owner: string, balance: number){
+    constructor(id: number, owner: string, balance: number) {
         this.id = id
         this.owner = owner
-        this.balance = balance
+        this._balance = balance
     }
 
-    deposit(amount: number): void{
-        if(amount <= 0)
+    deposit(amount: number): void {
+        if (amount <= 0)
             throw new Error('Invalid amount')
-        this.balance += amount
+        this._balance += amount
+    }
+
+    getBalance(): number { // a way for show private things
+        return this._balance
     }
 }
 
 let account = new Account(1, "Alireza", 0)
 account.deposit(100)
-console.log(account)
+console.log(account.getBalance)

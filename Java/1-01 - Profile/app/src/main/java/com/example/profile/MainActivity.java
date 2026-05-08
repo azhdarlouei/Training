452bcd0e64571aa41e61aa671aec5334ee12ae4f
Alireza <ar.azhdarloo@gmail.com>
2026-05-08 15:48:37 +0330

    Java - Profile (UNI): add onClick and Toast

--- Java/1-01 - Profile/app/src/main/java/com/example/profile/MainActivity.java
+++ Java/1-01 - Profile/app/src/main/java/com/example/profile/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.profile;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +24,55 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        ImageView profile = findViewById(R.id.imageView);
+        profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "this is profile",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Button name = findViewById(R.id.button);
+        name.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public  void  onClick(View v){
+                Toast.makeText(MainActivity.this, "this is user name",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        LinearLayout changeUserName = findViewById(R.id.linearLayout);
+        changeUserName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "this is a button for change user name", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        LinearLayout changeName = findViewById(R.id.linearLayout2);
+        changeName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "this is a button for change name", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        LinearLayout changePassword = findViewById(R.id.linearLayout3);
+        changePassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "this is a button for change password", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Button logOut = findViewById(R.id.button2);
+
+        logOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "log out button",Toast.LENGTH_SHORT).show();
+            }
         });
     }
 }

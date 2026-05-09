0f472030793cc935aa63976f7aaf0ae2c8c5ad31
Alireza <ar.azhdarloo@gmail.com>
2026-05-09 18:27:46 +0330

    TypeScript - OOP-Example: interfaces

---
+++ TypeScript/OOP-Example/src/calender.ts
@@ -0,0 +1,20 @@
+interface Calender {
+    name: string
+    addEvent(): void
+    removeEvent(): void
+}
+
+interface CloudCalender extends Calender {
+    sync(): void
+}
+
+class GoogleCalender implements Calender {
+    constructor(public name: string) { }
+    addEvent(): void {
+        throw new Error("Method not implemented.")
+    }
+    removeEvent(): void {
+        throw new Error("Method not implemented.")
+    }
+
+}

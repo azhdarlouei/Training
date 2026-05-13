ee7764f7307a0d18b21289e7eae4ea12d9a0051d
Alireza <ar.azhdarloo@gmail.com>
2026-05-13 16:29:53 +0330

    Node.js - Mongoose Demo: create schema

--- Node.js/Mongoose Demo/index.js
+++ Node.js/Mongoose Demo/index.js
@@ -3,3 +3,16 @@ const mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/firstDB')
     .then(() => console.log('connected to mongoDB...'))
     .catch((err => console.log(err)))
+
+const courseSchema = new mongoose.Schema({
+    name: String,
+    author: String,
+    tags: [String],
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    isPublished: Boolean
+})
+
+const Course = mongoose.model('Course', courseSchema)

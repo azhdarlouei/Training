7210df87b3a7924facf9b81aacf40e42de7c8980
Alireza <ar.azhdarloo@gmail.com>
2026-05-17 23:14:48 +0330

    Node.js - Mongoose Demo: comparison operators

--- Node.js/Mongoose Demo/index.js
+++ Node.js/Mongoose Demo/index.js
@@ -19,9 +19,10 @@ const Course = mongoose.model('Course', courseSchema)
 
 const createCourse = async () => {
     const course = new Course({
-        name: "Angular",
+        name: "Node JS",
         author: "Alireza",
-        tags: ['angular', 'ng'],
+        tags: ['node.js', 'npm'],
+        price: 900,
         isPublished: true
     })
 
@@ -34,20 +35,35 @@ const createCourse = async () => {
 
 
 const getCourse = async () => {
-    const courses = await Course.find({
-        author: "Alireza",
-        isPublished: true
-    })
+
+    // eq  (equal)
+    // ne  (not euqal)
+    // gt  (greater than)
+    // gte (greater than or equal)
+    // 1t  (less than)
+    // lte (less than or equal)
+    // in
+    // nin (not in)
+
+    const courses = await Course
+        // .find({
+        //     author: "Alireza",
+        //     isPublished: true
+        // })
+        .find({
+            $eq: 900
+        })
         .limit(10)
         .sort({
             name: 1
         })
         .select({
             name: 1,
-            tags: 1
+            tags: 1,
+            price: 1
         })
 
     console.log(courses)
 }
 
-getCourse()
+// getCourse()

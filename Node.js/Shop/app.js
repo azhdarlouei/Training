04521994af23a485e961c7dc3fe02fed6f2cf8fc
Alireza <ar.azhdarloo@gmail.com>
2026-05-21 17:13:37 +0330

    Node.js - Shop: initial project and add template

---
+++ Node.js/Shop/app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+app.set('views', 'views')
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'index.html'))
+})
+
+
+app.listen(3000, () => {
+    console.log('Running on port 3000....')
+})

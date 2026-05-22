48670015bdbe9f174a6e803d4a9f68bc42c01d36
Alireza <ar.azhdarloo@gmail.com>
2026-05-22 21:34:05 +0330

    Node.js - Shop: edit product

--- Node.js/Shop/controllers/admin.js
+++ Node.js/Shop/controllers/admin.js
@@ -1,13 +1,26 @@
 const Product = require('../models/product')
 
+exports.getProducts = (req, res) => {
+    Product.find()
+        .then(
+            products => {
+                res.render('admin/products', {
+                    products: products,
+                    pageTitle: 'Admin Products',
+                    path: '/admin/products'
+                })
+            })
+}
+
 exports.getAddProduct = (req, res) => {
-    res.render('admin/add-product',{
+    res.render('admin/add-product', {
         path: '/admin/add-product',
-        pageTitle: 'Add Product'
+        pageTitle: 'Add Product',
+        editing: false
     })
 }
 
-exports.postAddProduct = (req, res)=>{
+exports.postAddProduct = (req, res) => {
     const title = req.body.title
     const imageUrl = req.body.imageUrl
     const price = req.body.price
@@ -20,8 +33,54 @@ exports.postAddProduct = (req, res)=>{
         imageUrl: imageUrl
     })
     product.save()
-        .then(result=>{
+        .then(result => {
             console.log('Created Product')
+            res.redirect('/')
+        })
+}
+
+exports.getEditProduct = (req, res) => {
+    const editMode = req.query.edit
+
+    if (!editMode)
+        return res.redirect('/')
+
+    const id = req.params.productId
+
+    Product.findById(id)
+        .then(product => {
+            if (!product)
+                return res.redirect('/')
+
+            res.render('admin/add-product', {
+                pageTitle: 'Edit Product',
+                path: '/admin/edit-product',
+                editing: editMode,
+                product: product
+            })
+        })
+        .catch(err => {
+            console.log(err)
+        })
+}
+
+exports.postEditProduct = (req, res) => {
+    const id = req.body.id
+
+    const title = req.body.title
+    const imageUrl = req.body.imageUrl
+    const price = req.body.price
+    const description = req.body.description
+    Product.findById(id)
+        .then(product => {
+            product.title = title
+            product.price = price
+            product.description = description
+            product.imageUrl = imageUrl
+            return product.save()
+        })
+        .then(result=>{
+            console.log('product updated')
             res.redirect('/')
         })
 }
--- Node.js/Shop/routes/admin.js
+++ Node.js/Shop/routes/admin.js
@@ -6,5 +6,8 @@ const adminController = require('../controllers/admin')
 
 router.get('/add-product', adminController.getAddProduct)
 router.post('/add-product', adminController.postAddProduct)
+router.get('/products', adminController.getProducts)
+router.get('/edit-product/:productId', adminController.getEditProduct)
+router.post('/edit-product',adminController.postEditProduct)
 
 module.exports = router
